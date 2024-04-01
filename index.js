9f7c068d59f9ba461c19d88362798724f3a496aa
rimasultana <rimasultana20225@gmail.com>
2024-04-01 14:59:17 +0600

    some

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 document.getElementById("demo").style.color = "blue";
 document.getElementById("demo").style.textAlign = "center";
 document.getElementById("dem").style.fontSize = "larger";
-document.getElementById("demo").style.alignItems = "center";
